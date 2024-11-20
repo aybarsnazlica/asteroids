1a63422087c934e62caaa7862c386dce29e0dd59
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-11-20 16:32:35 +0900

    Print to screen

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import pygame
 
+from constants import *
+
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
